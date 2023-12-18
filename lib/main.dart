ae92ca84482005568230770aece1902616ac1d04
ayseturan02 <ayse0025turan@gmail.com>
2023-12-18 12:17:23 +0300

    drafting

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,211 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CodeCampApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CodeCampApp extends StatelessWidget {
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+       appBar: AppBar(
+         foregroundColor: Colors.white,
+         backgroundColor: Colors.black12,
+         title: const Text(
+             "title",
+         style: TextStyle(
+           fontSize: 32,
+           fontWeight: FontWeight.w400,
+         ),
+         ),
+         centerTitle: true,
+       ),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.black12,
+                child:  Row (
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                     const SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: Image(
+                        image: AssetImage(
+                          "assets/image/indir.png",
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 30.0,top: 2.0, right: 50.0, bottom: 0.0),
+                          child: Text(
+                              "flutter nedir",
+                            style: TextStyle(
+                                color: Colors.black,
+                            fontSize: 25,
+                            ),
+                          ),
+                        ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 100,
+                                width: 240,
+                                color: Colors.grey,
+                                child: const Text(
+                                  "   fnmsffsfkdsmfff",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 23,
+                                  ),
+                                ),
+                              ),
+                            ),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+                            Padding(
+                              padding: const EdgeInsets.only(left: 200.0,top: 2.0, right: 2.0, bottom: 2.0),
+                              child: IconButton(onPressed: (){} , icon: const Icon(
+                                size: 30,
+                                Icons.add_box_sharp,
+                                 ),
+                              ),
+                            ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.cyan.shade50,
+                child:  Row (
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: Image(
+                        image: AssetImage(
+                          "assets/image/dart.png",
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 30.0,top: 2.0, right: 50.0, bottom: 0.0),
+                          child: Text(
+                            "dart nedir",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 100,
+                            width: 240,
+                            color: Colors.grey,
+                            child: const Text(
+                              "   sbavabsvsf",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 23,
+                              ),
+                            ),
+                          ),
+                        ),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                        Padding(
+                          padding: const EdgeInsets.only(left: 200.0,top: 2.0, right: 2.0, bottom: 2.0),
+                          child: IconButton(onPressed: (){} , icon: const Icon(
+                            size: 30,
+                            Icons.account_balance_sharp,
+                          ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.cyan..shade900,
+                  child:  Row (
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image(
+                      image: AssetImage(
+                        "assets/image/dartf.png",
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 30.0,top: 2.0, right: 50.0, bottom: 0.0),
+                        child: Text(
+                          "ne yapılır",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 100,
+                          width: 240,
+                          color: Colors.grey,
+                          child: const Text(
+                            "   fmdmanmdnabvd",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 23,
+                            ),
+                          ),
+                        ),
+                      ),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+                      Padding(
+                        padding: const EdgeInsets.only(left: 200.0,top: 2.0, right: 2.0, bottom: 2.0),
+                        child: IconButton(onPressed: (){} , icon: const Icon(
+                          size: 30,
+                          Icons.add_a_photo_sharp,
+                        ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              ),
+            ],
+          ),
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
